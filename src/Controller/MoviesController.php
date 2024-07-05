3211d4013a5979d268fd6ec4a58c032ab9fc692e
RicardoGestiada <rgestiada12@gmail.com>
2024-07-05 18:33:56 +0800

    Installed Tailwindcss

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
-        $movies = ['Avengers : Endgame', 'Inception', 'Loki', 'Black Widow'];
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findBy([], ['id' => 'DESC']);
 
-        return $this->render('index.html.twig', array(
-            'movies' => $movies
-        ));
+        return $this->render('index.html.twig', $movies);
     }
 }
